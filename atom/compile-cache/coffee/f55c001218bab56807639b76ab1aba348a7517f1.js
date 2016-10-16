22bfe3596eb6497022a8e154018d9814fa3854d2
Paul Manning <mortman1@gmail.com>
2016-10-16 16:58:47 -0700

    adding some missing stuff

---
+++ atom/compile-cache/coffee/f55c001218bab56807639b76ab1aba348a7517f1.js
@@ -0,0 +1,8 @@
+(function() {
+
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL1VzZXJzL3BhdWwvLmF0b20vaW5pdC5jb2ZmZWUiCiAgXSwKICAibmFtZXMiOiBbXSwKICAibWFwcGluZ3MiOiAiQUFBQTtBQUFBO0FBQUEiCn0=
+
+//# sourceURL=/Users/paul/.atom/init.coffee
